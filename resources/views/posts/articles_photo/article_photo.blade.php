2d8eda562f800d7daa75c3068bb79acdc83b66ca
TheBestJB <thebestjb@gmail.com>
2016-02-29 14:46:16 +0100

    fix view

--- resources/views/posts/articles_photo/article_photo.blade.php
+++ resources/views/posts/articles_photo/article_photo.blade.php
@@ -1,6 +1,6 @@
 <div class="col-xs-6 col-md-3">
 	<a href="{{ $post->photo_id }}" class="thumbnail" style="text-align: center; height: 210px;">
-	<b><font color="#29d683">Photo {{ $post->Date }}</font></b>
+	<b><font color="#29d683">Photo</font></b>
 	<hr style="margin: 0px 0px 4px 0px">
 	<img src="pics/mini/{{ $post->photo_name }}" alt="{{ $post->photo_name }}">
 	</a>
