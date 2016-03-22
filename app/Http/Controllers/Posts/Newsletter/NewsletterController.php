4cdc528e3d840f11768a61767d3e4436fe785f33
thebestjb <thebestjb@gmail.com>
2016-03-22 03:11:46 +0100

    sending newsletters OK

--- app/Http/Controllers/Posts/Newsletter/NewsletterController.php
+++ app/Http/Controllers/Posts/Newsletter/NewsletterController.php
@@ -29,25 +29,25 @@ class NewsletterController extends Controller
     	return view('forms.newsletter.newsletter')->with('subscribers', $subscribers);
     }
     
+    /**
+     * Sends a newsletter to all subscribers.
+     * 
+     * @param NewsletterRequest $request
+     */
     public function postForm(NewsletterRequest $request)
     {
-    	/*$subs = Newsletter::all();
+    	$subs = Newsletter::all();
     	foreach ($subs as $sub)
     	{
-    		echo $sub->email_address;
-    		echo $sub->uniqid.'<br><br>';
-    	}*/
-    	
-    	$test = 'test';
-    	
-    	$to = 'thebestjb@gmail.com';
-    	$headers  = 'MIME-Version: 1.0' . "\r\n";
-    	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
-    	$message = $request->newsletter_body;
-    	$message .= '<br><a href="http://www.bleog.fr"><strong>Bleog.fr</strong></a><hr>
+    		$to = $sub->email_address;
+    		$headers  = 'MIME-Version: 1.0' . "\r\n";
+    		$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    		$message = $request->newsletter_body;
+    		$message .= '<br><a href="http://www.bleog.fr"><strong>Bleog.fr</strong></a><hr>
 						<small>Pour ne plus recevoir les newsletters, vous pouvez vous désinscrire en cliquant
-						<a href="http://www.bleog.fr/newsletter/unsuscribe/'.$test.'">ici</a></small>';
-    	mail($to, $request->newsletter_title, $message, $headers);
+						<a href="http://www.bleog.fr/newsletter/unsuscribe/'.$sub->uniqid.'">ici</a></small>';
+    		mail($to, $request->newsletter_title, $message, $headers);
+    	}
     	
     	return redirect()->route('newsletter')->with('success', 'La newsletter a bien été envoyée !');
     }
