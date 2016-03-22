24bde2d1d6f09f1e10a8d40c21377ded9923fa63
thebestjb <thebestjb@gmail.com>
2016-03-22 03:06:21 +0100

    test newsletter sending

--- app/Http/Controllers/Posts/Newsletter/NewsletterController.php
+++ app/Http/Controllers/Posts/Newsletter/NewsletterController.php
@@ -31,6 +31,24 @@ class NewsletterController extends Controller
     
     public function postForm(NewsletterRequest $request)
     {
+    	/*$subs = Newsletter::all();
+    	foreach ($subs as $sub)
+    	{
+    		echo $sub->email_address;
+    		echo $sub->uniqid.'<br><br>';
+    	}*/
+    	
+    	$test = 'test';
+    	
+    	$to = 'thebestjb@gmail.com';
+    	$headers  = 'MIME-Version: 1.0' . "\r\n";
+    	$headers .= 'Content-type: text/html; charset=iso-8859-1' . "\r\n";
+    	$message = $request->newsletter_body;
+    	$message .= '<br><a href="http://www.bleog.fr"><strong>Bleog.fr</strong></a><hr>
+						<small>Pour ne plus recevoir les newsletters, vous pouvez vous désinscrire en cliquant
+						<a href="http://www.bleog.fr/newsletter/unsuscribe/'.$test.'">ici</a></small>';
+    	mail($to, $request->newsletter_title, $message, $headers);
+    	
     	return redirect()->route('newsletter')->with('success', 'La newsletter a bien été envoyée !');
     }
 }
