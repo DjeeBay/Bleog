ccd9a927dce7a63ed09d67910db4edb22f9e347f
thebestjb <thebestjb@gmail.com>
2018-01-20 15:02:20 +0100

    Fix styles.

--- resources/views/forms/posts/photos/photos.blade.php
+++ resources/views/forms/posts/photos/photos.blade.php
@@ -7,5 +7,7 @@
 @section('content')
     <h1 class="text-center">Upload de photos</h1>
     <hr>
-    <photos-upload :show-photos="true"></photos-upload>
+    <div class="container">
+        <photos-upload :show-photos="true"></photos-upload>
+    </div>
 @stop
